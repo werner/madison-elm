3b261f8ea6275f83ff455393f087c2d223ccc988
Werner <werner_a_e@yahoo.es>
2016-12-09 14:19:55 -0400

    use material design grid

--- src/elm/Components/Login/Form.elm
+++ src/elm/Components/Login/Form.elm
@@ -3,15 +3,27 @@ module Components.Login.Form exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
+import Material.Grid as MG exposing (..) 
+
 import Components.Login.Messages exposing (..)
 import Components.Login.Models exposing (..)
 
 view : User -> Html Msg
 view user = 
-    div [ class "m3" ]
-        [ h1 [] [ text "Log In" ]
-        , Html.form [] 
-        [ input [ type_ "text", placeholder "Email", onInput Email ] []
-        , input [ type_ "password", placeholder "Password", onInput Password ] []
-        , button [ class "btn", onClick ( GoToLogin user.email user.password ) ] [ text "Login" ] ] ]
+    grid []
+         [ cell [ MG.size All 12 ] [ h1 [] [ text "Log In" ] ]
+         , cell [ MG.size All 12 ] [ 
+             Html.form [] 
+                 [ grid []
+                        [ cell [ MG.size All 8 ] 
+                               [ input [ type_ "text", placeholder "Email", onInput Email ] [] ]
+                        , cell [ MG.size All 8 ] 
+                               [ input [ type_ "password", placeholder "Password", onInput Password ] [] ]
+                        , cell [ MG.size All 8 ] 
+                               [ button [ class "btn", onClick ( GoToLogin user.email user.password ) ] 
+                                        [ text "Login" ] ] 
+                        ]
+                 ] 
+             ]
+         ]
 
