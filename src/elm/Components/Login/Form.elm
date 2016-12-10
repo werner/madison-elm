8864a01b8b22e14329f727a7f9edbc4e4d526a15
Werner <werner_a_e@yahoo.es>
2016-12-10 12:42:04 -0400

    transform the login form to a materialize view

--- src/elm/Components/Login/Form.elm
+++ src/elm/Components/Login/Form.elm
@@ -1,16 +1,28 @@
 module Components.Login.Form exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (class, src, type_)
 import Html.Events exposing (onInput, onClick)
 import Components.Login.Messages exposing (..)
 import Components.Login.Models exposing (..)
 
 view : User -> Html Msg
 view user = 
-    div [ class "m3" ]
-        [ h1 [] [ text "Log In" ]
-        , Html.form [] 
-        [ input [ type_ "text", placeholder "Email", onInput Email ] []
-        , input [ type_ "password", placeholder "Password", onInput Password ] []
-        , button [ class "btn", onClick ( GoToLogin user.email user.password ) ] [ text "Login" ] ] ]
+    div [ class "row" ]
+        [ div [ class "col s12 m3" ] 
+              [ div [ class "card" ] 
+                    [ div [ class "card-image" ] 
+                          [ img  [ src "static/img/logo.png" ] [] ] 
+                    , div [ class "card-content" ]
+                          [ div [ class "input-field" ] 
+                                [ input [ type_ "text", onInput Email, class "validate" ] []
+                                , label [] [ text "Email" ] ]
+                          , div [ class "input-field" ] 
+                                [ input [ type_ "password", onInput Password, class "validate" ] [] 
+                                , label [] [ text "Password" ] ]
+                          ]
+                    , div [ class "card-action" ]
+                          [ button [ class "btn", onClick ( GoToLogin user.email user.password ) ] [ text "Login" ] ]
+                    ] 
+              ] 
+        ]
--- src/static/index.js
+++ src/static/index.js
@@ -1,10 +1,11 @@
 // pull in desired CSS/SASS files
+require('materialize-css/dist/css/materialize.min.css');
+require('font-awesome/css/font-awesome.css');
+
 require( './styles/main.scss' );
 var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
 require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
-
-require('ace-css/css/ace.css');
-require('font-awesome/css/font-awesome.css');
+require( 'materialize-css/dist/js/materialize.min.js' );
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
