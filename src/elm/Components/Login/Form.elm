1aae4d5589b60ceb49b598df1b3c161c4f095d4f
Werner <werner_a_e@yahoo.es>
2016-12-10 06:55:55 -0400

    add logo and a card for login

--- src/elm/Components/Login/Form.elm
+++ src/elm/Components/Login/Form.elm
@@ -6,32 +6,47 @@ import Material.Color as Color
 import Material.Grid as Grid exposing (..) 
 import Material.Button as Button exposing (render, onClick, raised)
 import Material.Textfield as Textfield
+import Material.Card as Card
+import Material.Options as Options exposing (cs, css)
 
 import Components.Login.Messages exposing (..)
 import Components.Login.Models exposing (..)
 
+
+white : Options.Property c m 
+white = 
+  Color.text Color.white 
+
 view : User -> Html Msg
-view user = Material.Scheme.topWithScheme Color.Teal Color.LightGreen <|
-    grid []
-         [ cell [ Grid.size All 12 ] [ h1 [] [ text "Log In" ] ]
-         , cell [ Grid.size All 12 ] [ 
-             Html.form [] 
-                 [ grid []
-                        [ cell [ Grid.size All 8 ] 
-                               [ Textfield.render Mdl [0] user.mdl 
+view user = Material.Scheme.topWithScheme Color.Teal Color.LightGreen <| form user
+
+form : User -> Html Msg
+form user = 
+    Card.view [ css "width" "256px" ] 
+              [ Card.title
+                  [ css "background" "url('static/img/logo.png') center / cover"
+                  , css "height" "256px"
+                  , css "padding" "0"
+                  ] 
+                  [ Card.head 
+                      [ white 
+                      , Options.scrim 0.75
+                      , css "padding" "16px"
+                      , css "width" "100%"
+                      ]
+                      [ text "Login" ]
+                  ]
+              , Card.actions []
+                  [ Textfield.render Mdl [0] user.mdl 
                                   [ Textfield.label "Email"
-                                  , Textfield.floatingLabel ] ]
-                        , cell [ Grid.size All 8 ] 
-                               [ Textfield.render Mdl [2] user.mdl 
+                                  , Textfield.floatingLabel ] 
+                  , Textfield.render Mdl [2] user.mdl 
                                   [ Textfield.label "Password"
                                   , Textfield.floatingLabel
-                                  , Textfield.password ] ]
-                        , cell [ Grid.size All 8 ] 
-                               [ Button.render Mdl [3] user.mdl 
+                                  , Textfield.password ]
+                  , Button.render Mdl [3] user.mdl 
                                   [ Button.colored
-                                  , Button.onClick ( GoToLogin user.email user.password ) ] [ text "Login" ] ] 
-                        ]
-                 ] 
-             ]
-         ]
-
+                                  , Button.onClick ( GoToLogin user.email user.password ) ] 
+                                  [ text "Login" ]
+                  ]
+              ]
