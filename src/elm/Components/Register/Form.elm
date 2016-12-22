fe34f6e8697b4707e977b307f8a61784b99dc4cc
Werner <werner_a_e@yahoo.es>
2016-12-22 15:30:29 -0400

    add styling to register form

--- src/elm/Components/Register/Form.elm
+++ src/elm/Components/Register/Form.elm
@@ -1,53 +1,59 @@
 module Components.Register.Form exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, src, type_, id, for, href)
+import Html.Attributes as Attr
 import Html.Events exposing (onInput, onClick)
+import Html.CssHelpers
+
+import MainCss exposing (..)
 import Components.Register.Messages exposing (..)
 import Components.Register.Models exposing (..)
 
+{ id, class, classList } =
+    Html.CssHelpers.withNamespace "madison"
+
 view : RegUser -> Html Msg
 view user = 
-    div [ class "container" ]
-        [ div [ class "row" ]
-                [ div [ class "col s12 l6 offset-l3" ] 
-                      [ div [ class "card-panel" ] 
-                            [ div [ class "row center valign-wrapper" ]
-                                  [ div [ class "col s2"] [ img  [ src "static/img/logo-nav.png" ] [] ]
-                                  , div [ class "col s10 left-align "] [ div  [] [ h2 [] [ text "Register" ] ] ] ]
-                            , div [ class "row" ]
-                                  [ div [ class "input-field" ] 
-                                        [ input [ type_ "text", onInput Email, class "validate" ] []
+    div [ Attr.class "container" ]
+        [ div [ Attr.class "row" ]
+                [ div [ Attr.class "col s12 l6 offset-l3" ] 
+                      [ div [ Attr.class "card-panel" ] 
+                            [ div [ Attr.class "row center valign-wrapper" ]
+                                  [ div [ Attr.class "col s2"] [ img  [ Attr.src "static/img/logo-nav.png" ] [] ]
+                                  , div [ Attr.class "col s10 left-align "] [ div  [] [ h2 [] [ text "Register" ] ] ] ]
+                            , div [ Attr.class "row" ]
+                                  [ div [ class [ InputField ], Attr.class "input-field" ] 
+                                        [ input [ Attr.type_ "text", onInput Email, Attr.class "validate" ] []
                                         , label [] [ text "Email" ] ]
                                   ]
-                            , div [ class "row" ]
-                                  [ div [ class "input-field" ] 
-                                        [ input [ type_ "password", onInput Password, class "validate" ] [] 
+                            , div [ Attr.class "row" ]
+                                  [ div [ class [ InputField ], Attr.class "input-field" ] 
+                                        [ input [ Attr.type_ "password", onInput Password, Attr.class "validate" ] [] 
                                         , label [] [ text "Password" ] ]
                                   ]
-                            , div [ class "row" ]
-                                  [ div [ class "input-field" ] 
-                                        [ input [ type_ "password", 
-                                                  onInput PasswordConfirmation, class "validate" ] [] 
+                            , div [ Attr.class "row" ]
+                                  [ div [ class [ InputField ], Attr.class "input-field" ] 
+                                        [ input [ Attr.type_ "password", 
+                                                  onInput PasswordConfirmation, Attr.class "validate" ] [] 
                                         , label [] [ text "Password Confirmation" ] ]
                                   ]
-                            , div [ class "row" ]
-                                  [ div [ class "input-field" ] 
-                                        [ input [ type_ "text", onInput FirstName, class "validate" ] []
+                            , div [ Attr.class "row" ]
+                                  [ div [ class [ InputField ], Attr.class "input-field" ] 
+                                        [ input [ Attr.type_ "text", onInput FirstName, Attr.class "validate" ] []
                                         , label [] [ text "First Name" ] ]
                                   ]
-                            , div [ class "row" ]
-                                  [ div [ class "input-field" ] 
-                                        [ input [ type_ "text", onInput LastName, class "validate" ] []
+                            , div [ Attr.class "row" ]
+                                  [ div [ class [ InputField ], Attr.class "input-field" ] 
+                                        [ input [ Attr.type_ "text", onInput LastName, Attr.class "validate" ] []
                                         , label [] [ text "Last Name" ] ]
                                   ]
-                            , div [ class "row" ]
-                                  [ div [ class "input-field" ] 
-                                        [ input [ type_ "text", onInput CompanyName, class "validate" ] []
+                            , div [ Attr.class "row" ]
+                                  [ div [ class [ InputField ], Attr.class "input-field" ] 
+                                        [ input [ Attr.type_ "text", onInput CompanyName, Attr.class "validate" ] []
                                         , label [] [ text "Company Name" ] ]
                                   ]
-                            , div [ class "row" ]
-                                  [ button [ class "btn waves-effect waves-light col s12"
+                            , div [ Attr.class "row" ]
+                                  [ button [ Attr.class "btn waves-effect waves-light col s12"
                                            , onClick ( GoToRegister user ) ] [ text "Register" ] ]
                             ] 
                       ] 
