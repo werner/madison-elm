18a55086725094aae68201bde816f332a72b61b9
Werner <werner_a_e@yahoo.es>
2017-03-13 14:01:30 -0400

    removed edit warehouse module

--- src/elm/Components/Warehouses/Routing.elm
+++ src/elm/Components/Warehouses/Routing.elm
@@ -5,10 +5,8 @@ import UrlParser exposing (..)
 
 type WarehouseRoute = WarehouseList
                     | WarehouseNew
-                    | WarehouseShow WarehouseId
 
 matcherWarehouse : (WarehouseRoute -> b) -> List (Parser (b -> c) c)
 matcherWarehouse route = [ map (route WarehouseList) (s "warehouses")
                          , map (route WarehouseNew) (s "warehouses" </> s "new")
-                         , map (\x -> route (WarehouseShow x)) (s "warehouses" </> int </> s "edit")
                          ]
--- src/elm/Components/Warehouses/View.elm
+++ src/elm/Components/Warehouses/View.elm
@@ -6,7 +6,6 @@ import ViewHelpers exposing (notFoundView)
 import Components.Warehouses.Models exposing ( WarehouseId, Warehouse, WarehouseModel )
 import Components.Warehouses.Messages exposing (Msg(..))
 import Components.Warehouses.List as List exposing (view)
-import Components.Warehouses.Edit as Edit exposing (view)
 import Components.Warehouses.New  as New exposing (view)
 import Components.Warehouses.Routing exposing (WarehouseRoute(..))
 
@@ -15,19 +14,3 @@ view token msg ({ form, errors, warehouse, warehouses } as model) =
     case msg of
         WarehouseList    -> List.view token model
         WarehouseNew     -> New.view <| New.body model
-        WarehouseShow id -> warehouseEditPage warehouses id
-
-warehouseEditPage : List Warehouse -> WarehouseId -> Html Msg
-warehouseEditPage warehouses warehouseId = 
-    let
-        maybeWarehouse =
-            warehouses
-                |> List.filter (\warehouse -> warehouse.id == (Just warehouseId))
-                |> List.head
-    in
-        case maybeWarehouse of
-            Just warehouse ->
-                Edit.view warehouse
-
-            Nothing ->
-                notFoundView
